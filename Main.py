eb9eec7fe74c0d3a05ccb8045c3852441cd056a3
KristopherJH <kristopher.hoover@mun.ca>
2017-07-23 12:00:44 -0230

    Corrected video naming convention and started main fcn

---
+++ Main.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+# Vector of video files for project:
+vids = ['Videos/1-A.mp4', 'Videos/1-B.mp4', 'Videos/2-A.mp4', 'Videos/2-B.mp4'/
+        'Videos/3-A.mp4', 'Videos/3-B.mp4', 'Videos/4-A.mp4', 'Videos/4-B.mp4'/
+        'Videos/5-A.mp4', 'Videos/5-B.mp4', 'Videos/Varying']
+
